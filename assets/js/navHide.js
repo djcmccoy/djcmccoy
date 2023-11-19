058c90be26f90d9c3f9d5718720880419b6d08f4
djcmccoy <55002251+djcmccoy@users.noreply.github.com>
2023-11-19 04:54:41 -0500

    Update navHide.js

--- assets/js/navHide.js
+++ assets/js/navHide.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     var navbar = document.getElementById("navbar");
     var hamburgerButton = document.querySelector(".hamburger");
+    var themeToggle = document.getElementById("themeToggle");
 
     hamburgerButton.addEventListener("click", function() {
         navbar.classList.toggle("hidden");
+        // Add or remove the theme-invert class based on the navbar's visibility
+        themeToggle.classList.toggle("theme-invert", !navbar.classList.contains("hidden"));
     });
 });
