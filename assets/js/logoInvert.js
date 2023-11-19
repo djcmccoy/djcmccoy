894369d89408c41e265d07311dc7c8f1aa64ad06
djcmccoy <55002251+djcmccoy@users.noreply.github.com>
2023-11-19 06:02:41 -0500

    Update logoInvert.js

--- assets/js/logoInvert.js
+++ assets/js/logoInvert.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     var navbar = document.getElementById("navbar");
+    var hamburgerButton = document.querySelector(".hamburger");
     var themeToggle = document.getElementById("themeToggle");
+
+    hamburgerButton.addEventListener("click", function() {
+        navbar.classList.toggle("hidden");
+    });
 
     themeToggle.addEventListener("click", function() {
         // Add or remove the theme-invert-logo class based on the navbar's visibility
