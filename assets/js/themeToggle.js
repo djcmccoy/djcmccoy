f0b37f3757e1fe5144b90ffef493dded574809ff
djcmccoy <55002251+djcmccoy@users.noreply.github.com>
2023-11-20 11:34:24 -0500

    Update themeToggle.js

--- assets/js/themeToggle.js
+++ assets/js/themeToggle.js
@@ -3,7 +3,9 @@ if (localStorage.getItem('theme')) {
     document.body.dataset.theme = localStorage.getItem('theme');
 }
 
-document.getElementById('themeToggle').addEventListener('click', function() {
+const themeToggle = document.getElementById('themeToggle');
+
+themeToggle.addEventListener('click', function () {
     let currentTheme = document.body.dataset.theme;
 
     if (currentTheme === 'dark') {
@@ -13,15 +15,10 @@ document.getElementById('themeToggle').addEventListener('click', function() {
         document.body.dataset.theme = 'dark';
         localStorage.setItem('theme', 'dark');
     }
-});
 
-// Add the following code to hide the theme toggle when the menu is open
-document.querySelector('.hamburger').addEventListener('click', function() {
-    document.body.classList.toggle('menu-open');
-
-    // You may want to add additional logic to handle other aspects of your menu
-    // ...
-
-    // Add or remove the class on the theme toggle button based on menu state
-    document.getElementById('themeToggle').classList.toggle('menu-open', document.body.classList.contains('menu-open'));
+    // Add this section for smooth transition
+    themeToggle.style.pointerEvents = 'none';
+    setTimeout(() => {
+        themeToggle.style.pointerEvents = 'auto';
+    }, 1000); // Adjust the delay as needed
 });
