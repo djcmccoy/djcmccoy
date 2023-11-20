d0bbd35fa03ed58c63080d54993608400715b5d8
djcmccoy <55002251+djcmccoy@users.noreply.github.com>
2023-11-20 09:29:21 -0500

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,9 +1,4 @@
-/ On page load, set the theme from local storage
-if (localStorage.getItem('theme')) {
-    document.body.dataset.theme = localStorage.getItem('theme');
-}
-
-document.getElementById('themeToggle').addEventListener('click', function() {
+document.getElementById('themeToggle').addEventListener('click', function () {
     let currentTheme = document.body.dataset.theme;
 
     if (currentTheme === 'dark') {
@@ -13,4 +8,8 @@ document.getElementById('themeToggle').addEventListener('click', function() {
         document.body.dataset.theme = 'dark';
         localStorage.setItem('theme', 'dark');
     }
+});
+
+document.querySelector('.hamburger').addEventListener('click', function () {
+    document.body.dataset.menuOpen = (document.body.dataset.menuOpen !== 'true').toString();
 });
