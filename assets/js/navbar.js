2a6f26929b8dec4ddd0aba53a03b131b30720a04
djcmccoy <55002251+djcmccoy@users.noreply.github.com>
2023-11-20 09:23:50 -0500

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,17 +1,16 @@
-document.getElementById('themeToggle').addEventListener('click', function () {
+/ On page load, set the theme from local storage
+if (localStorage.getItem('theme')) {
+    document.body.dataset.theme = localStorage.getItem('theme');
+}
+
+document.getElementById('themeToggle').addEventListener('click', function() {
     let currentTheme = document.body.dataset.theme;
 
     if (currentTheme === 'dark') {
         document.body.dataset.theme = 'light';
-        document.body.dataset.menuOpen === 'true' ? document.body.dataset.logoInvert = 'light' : document.body.dataset.logoInvert = 'dark';
         localStorage.setItem('theme', 'light');
     } else {
         document.body.dataset.theme = 'dark';
-        document.body.dataset.menuOpen === 'true' ? document.body.dataset.logoInvert = 'dark' : document.body.dataset.logoInvert = 'light';
         localStorage.setItem('theme', 'dark');
     }
-});
-
-document.querySelector('.hamburger').addEventListener('click', function () {
-    document.body.dataset.menuOpen = (document.body.dataset.menuOpen !== 'true').toString();
 });
