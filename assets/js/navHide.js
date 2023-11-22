8b2bee0474201bed2b86845d3a2d53a5ebbe6c64
djcmccoy <55002251+djcmccoy@users.noreply.github.com>
2023-11-22 11:31:14 -0500

    Update navHide.js

--- assets/js/navHide.js
+++ assets/js/navHide.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     var navbar = document.getElementById("navbar");
+    var menu = document.getElementById("menu"); // Assuming you have a menu element
     var hamburgerButton = document.querySelector(".hamburger");
     var themeToggle = document.getElementById("themeToggle");
 
     hamburgerButton.addEventListener("click", function() {
         navbar.classList.toggle("hidden");
+        menu.classList.toggle("active"); // Toggle the visibility or position of the menu
+
         // Add or remove the theme-invert class based on the navbar's visibility
         themeToggle.classList.toggle("theme-invert", !navbar.classList.contains("hidden"));
     });
