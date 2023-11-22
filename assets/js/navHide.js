ee4fa6854c06e10ff1daf7dbc5d6fffde1fc159c
djcmccoy <55002251+djcmccoy@users.noreply.github.com>
2023-11-22 12:23:01 -0500

    Update navHide.js

--- assets/js/navHide.js
+++ assets/js/navHide.js
@@ -5,9 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
     var hamburgerButton = document.getElementById("hamburgerButton");
 
     hamburgerButton.addEventListener("click", function() {
-        // Toggle 'fade-out' class for elements within dropdown-nav
-        dropdownNav.classList.toggle("fade-out");
+        // Toggle 'fade-out' class for logo and theme toggle
         logo.classList.toggle("fade-out");
         themeToggle.classList.toggle("fade-out");
+
+        // Toggle 'background-fade-out' class for the entire dropdown-nav
+        dropdownNav.classList.toggle("background-fade-out");
     });
 });
