360a03575959c85db7dd97f37c52ffdf9de93581
djcmccoy <55002251+djcmccoy@users.noreply.github.com>
2023-11-23 03:20:13 -0500

    Update themeToggle.js

--- assets/js/themeToggle.js
+++ assets/js/themeToggle.js
@@ -14,3 +14,38 @@ document.getElementById('themeToggle').addEventListener('click', function() {
         localStorage.setItem('theme', 'dark');
     }
 });
+
+
+/* SWIPER */
+
+document.getElementById('themeToggle').addEventListener('click', function() {
+    let currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme === 'dark') {
+        document.body.classList.remove('dark-theme');
+        localStorage.setItem('theme', 'light');
+        // Set light theme styles for the button
+        updateSwiperPaginationBulletActiveColor('#ColorForLightTheme'); 
+    } else {
+        document.body.classList.add('dark-theme');
+        localStorage.setItem('theme', 'dark');
+        // Set dark theme styles for the button
+        updateSwiperPaginationBulletActiveColor('#110f10'); 
+    }
+});
+
+// On page load, set the theme from local storage
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-theme');
+    updateSwiperPaginationBulletActiveColor('#110f10');
+} else {
+    // Ensure light theme styles are set
+    updateSwiperPaginationBulletActiveColor('#ColorForLightTheme');
+}
+
+function updateSwiperPaginationBulletActiveColor(color) {
+    var elements = document.querySelectorAll('.swiper-pagination-bullet-active');
+    elements.forEach(function(element) {
+        element.style.background = color;
+    });
+}
